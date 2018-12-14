6eae204a91b48e41d9471991a6831b46895ad5b2
Sraw <lzyl888@gmail.com>
2018-12-14 13:32:59 -0500

    delete unused files, add test cases

--- Model/src/test/scala/StaticModelSpec.scala
+++ Model/src/test/scala/StaticModelSpec.scala
@@ -1,7 +1,7 @@
 import akka.actor.{ActorSystem, Props}
 import akka.http.scaladsl.model._
 import akka.testkit.{ImplicitSender, TestActorRef, TestKit}
-import com.csye7200.model.Movie
+import com.csye7200.model.{Movie, MovieInfo, MovieResponse}
 import com.csye7200.model.backend.StaticModel
 import com.typesafe.config.ConfigFactory
 import akka.pattern.ask
@@ -16,6 +16,18 @@ import scala.util.Success
 class StaticModelSpec() extends TestKit(ActorSystem("StaticModelSpec")) with ImplicitSender
   with WordSpecLike with Matchers with BeforeAndAfterAll {
 
+  def GetResult(path: String): MovieResponse = {
+    val getDataSource: String => RequestEntity => HttpResponse =
+      entity => { _ => HttpResponse(entity = entity, headers = List(headers.`Content-Type`(ContentTypes.`application/json`))) }
+    val fileStream = getClass.getResourceAsStream(path)
+    val entity = Source.fromInputStream(fileStream).mkString
+    val staticModel = TestActorRef(Props(classOf[StaticModel], ConfigFactory.empty(), Some(getDataSource(entity))))
+    implicit val timeout: Timeout = 3.seconds
+    val future = staticModel ? Movie("asdfasd")
+    val Success(result) = future.mapTo[MovieResponse].value.get
+    result
+  }
+
   override def afterAll: Unit = {
     TestKit.shutdownActorSystem(system)
   }
@@ -23,15 +35,13 @@ class StaticModelSpec() extends TestKit(ActorSystem("StaticModelSpec")) with Imp
   "StaticModel actor" must {
 
     "work with response1.json" in {
-      val getDataSource: String => RequestEntity => HttpResponse =
-        entity => {_ => HttpResponse(entity = entity , headers = List(headers.`Content-Type`(ContentTypes.`application/json`)))}
-      val fileStream = getClass.getResourceAsStream("response1.json")
-      val entity = Source.fromInputStream(fileStream).mkString
-      val staticModel = TestActorRef(Props(classOf[StaticModel], ConfigFactory.empty(), Some(getDataSource(entity))))
-      implicit val timeout: Timeout = 3.seconds
-      val future = staticModel ? Movie("asdfasd")
-      val Success(result) = future.value.get
-      result should be(List())
+      val result = GetResult("response1.json")
+      result shouldBe MovieResponse(MovieInfo("Test", List(), "", "", 100, 8.9, 1000, 1980), List())
+    }
+
+    "work with response2.json" in {
+      val result: MovieResponse = GetResult("response2.json")
+      result.related.length shouldBe 6
     }
 
   }
