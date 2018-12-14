63bdf032cb7e63e9a1ae3870b091dac3a32ca915
lingfeng.zhou <zhou.ling@husky.neu.edu>
2018-12-13 20:26:18 -0500

    reduce number of actions from 3 to 1, runtime 3s -> 0.5s

--- DataCenter/src/main/scala/com/csye7200/datacenter/GetResults.scala
+++ DataCenter/src/main/scala/com/csye7200/datacenter/GetResults.scala
@@ -1,34 +1,43 @@
 package com.csye7200.datacenter
 
+import java.time.LocalDateTime
+
 import org.apache.spark.sql._
 import org.apache.spark.sql.functions._
 
 object GetResults {
-
+  val spark = SparkSession.builder
+    .master("local")
+    .appName("getmovies")
+    .getOrCreate()
+  spark.sparkContext.setLogLevel("ERROR")
+  import spark.implicits._
+  val df = spark.read.parquet("DataCenter/src/main/resources/result.parquet").cache()
   def getmovies(title: String, limit: Int): String = {
-    val spark = SparkSession.builder
-      .master("local")
-      .appName("getmovies")
-      .getOrCreate()
-    spark.sparkContext.setLogLevel("ERROR")
-    import spark.implicits._
-    //read result
-    val df = spark.read.parquet("DataCenter/src/main/resources/result.parquet")
-    //get cluster
-    val cluster = df.filter(row => row.get(0)==title).select("prediction").head().get(0)
-    //sort with distance
-    val related_df = df.filter(s"prediction == $cluster").sort(asc("distanceFromCenter"))
-    //select required rows
-    val movie_df = related_df.select("primaryTitle", "genresSeq", "director","1actor","rms",
-        "averageRating", "numVotes","syear")
 
-    val newNames = Seq("title", "genres", "director", "actor", "length", "score", "voteNumber", "year")
+    //read result  select required rows
+
+    val movie_df = df.select("primaryTitle", "genresSeq", "director","1actor","rms",
+      "averageRating", "numVotes","syear","prediction","distanceFromCenter")
+
+    val newNames = Seq("title", "genres", "director", "actor", "length", "score", "voteNumber",
+      "year","prediction","distanceFromCenter")
+
     val dfRenamed = movie_df.toDF(newNames: _*)
-    dfRenamed.printSchema
-    val related = dfRenamed.limit(limit).toJSON.collect.mkString("\"related\": [", "," , "]" )
-    val origin = dfRenamed.filter(row => row.get(0)==title).toJSON.collect()(0)
+    //get cluster
+    val cluster = dfRenamed.select("title","prediction").filter($"title"===title).limit(1)("prediction")
+    //sort with distance
+    val related_df: Dataset[Row] = dfRenamed.filter($"prediction" === cluster).sort(asc("distanceFromCenter"))
 
-    "{ \"origin\": ".concat(origin).concat(",").concat(related).concat(" }")
+    val all = related_df.filter($"prediction" === cluster)
+      .drop("prediction","distanceFromCenter")
+    val related = all.filter($"title"=!=title).limit(limit)
+    val origin_df = all.filter($"title"===title)
+    val res = origin_df.union(related).toJSON.collect()
+    val origin_json = res(0)
+    val related_json = res.drop(1).mkString("\"related\": [", "," , "]" )
+
+    "{ \"origin\": ".concat(origin_json).concat(",").concat(related_json).concat(" }")
 
   }
 
