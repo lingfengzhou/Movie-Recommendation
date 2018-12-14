d413e3872df8e8c9bbd08f3c35b68a0b05ec602c
lingfeng.zhou <zhou.ling@husky.neu.edu>
2018-12-13 20:26:42 -0500

    add spark init

--- DataCenter/src/main/scala/com/csye7200/datacenter/MovieServer.scala
+++ DataCenter/src/main/scala/com/csye7200/datacenter/MovieServer.scala
@@ -4,6 +4,7 @@ import akka.actor.{ActorRef, ActorSystem}
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.server.Route
 import akka.stream.ActorMaterializer
+import org.apache.spark.sql.SparkSession
 
 import scala.concurrent.Await
 import scala.concurrent.duration.Duration
@@ -18,16 +19,20 @@ object MovieServer extends App with ClassificationRoutes {
   //#server-bootstrapping
 
   val classificationActor: ActorRef = system.actorOf(ClassificationActor.props, "classificationActor")
-
+  val spark = SparkSession.builder
+    .master("local")
+    .appName("getmovies")
+    .getOrCreate()
+  spark.sparkContext.setLogLevel("ERROR")
   //#main-class
   // from the UserRoutes trait
   lazy val routes: Route = classificationRoutes
   //#main-class
 
   //#http-server
-  Http().bindAndHandle(routes, "0.0.0.0", 8080)
+  Http().bindAndHandle(routes, "0.0.0.0", 8278)
 
-  println(s"Server online at http://0.0.0.0:8080/")
+  println(s"Server online at http://0.0.0.0:8278/")
 
   Await.result(system.whenTerminated, Duration.Inf)
   //#http-server
