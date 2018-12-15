1ac8e443015c68cf63d8cf3faf7861bb2671d4ed
lingfeng.zhou <zhou.ling@husky.neu.edu>
2018-12-15 17:25:46 -0500

    add conf

--- DataCenter/src/test/scala/com/csye7200/datacenter/RoutesSpec.scala
+++ DataCenter/src/test/scala/com/csye7200/datacenter/RoutesSpec.scala
@@ -1,9 +1,14 @@
 package com.csye7200.datacenter
 
 import akka.actor.ActorRef
-import akka.http.scaladsl.testkit.ScalatestRouteTest
+import akka.http.javadsl.model.StatusCodes
+import akka.http.scaladsl.marshalling.Marshal
+import akka.http.scaladsl.model.{HttpHeader, HttpRequest, MessageEntity}
+import akka.http.scaladsl.testkit.{RouteTestTimeout, ScalatestRouteTest}
 import org.scalatest.{Matchers, WordSpec}
 import org.scalatest.concurrent.ScalaFutures
+import scala.concurrent.duration._
+import akka.testkit.TestDuration
 
 class RoutesSpec extends WordSpec with Matchers with ScalaFutures with ScalatestRouteTest
   with ClassificationRoutes {
@@ -14,4 +19,15 @@ class RoutesSpec extends WordSpec with Matchers with ScalaFutures with Scalatest
   //todo "be able to update (POST /movies/update-classification)"
 
   //todo get movie result(title)
+//  "GetMovieRoutes" should {
+//    "return ok"in {
+//    val info = Info("Jana, the Girl from the Bohemian Forest",3)
+//    val infoEntity = Marshal(info).to[MessageEntity].futureValue
+//
+//    val request = Post("/movies/element/getMovie").withEntity(infoEntity)
+//    request ~> routes ~> check{
+//      status should === (StatusCodes.OK)
+//    }
+//  }
+//  }
 }
